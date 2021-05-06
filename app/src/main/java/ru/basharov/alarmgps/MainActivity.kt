999c42be004b3cc7c0550b49f88fc6edb8097c8c
Pirozhochk <lalka_lalka200@mail.ru>
2021-05-06 17:52:53 +0300

    Добавление двух функций: нахождение новой локации, сохранение старой

--- app/src/main/java/ru/basharov/alarmgps/MainActivity.kt
+++ app/src/main/java/ru/basharov/alarmgps/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.basharov.alarmgps
 
+import android.Manifest
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
@@ -8,11 +9,15 @@ import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import android.widget.TimePicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +33,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var pi:PendingIntent
 
     private var PERMISSION_ID = 52
+    lateinit var fusedLPC: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var btngps: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +47,9 @@ class MainActivity : AppCompatActivity() {
         alarmTxt.setText(R.string.txtAlarm)
         btnStart = findViewById(R.id.StartAlarm)
         btnEnd = findViewById(R.id.EndAlarm)
+        btngps = findViewById(R.id.btnGPS)
+
+        fusedLPC = LocationServices.getFusedLocationProviderClient(this)
 
         var calendar: Calendar = Calendar.getInstance()
         var myIntent: Intent = Intent(this, AlarmReceiver::class.java)
@@ -81,22 +92,77 @@ class MainActivity : AppCompatActivity() {
             myIntent.putExtra("extra","off")
             sendBroadcast(myIntent)
         }
+
+        btngps.setOnClickListener{
+            getLastLocation() }
     }
 
     private fun checkPermission(): Boolean{
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             return true
         }
         return false
     }
 
     private fun RequestPermission(){
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_ID)
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_ID)
     }
 
     private fun isLocationEnabled(): Boolean{
-        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun getLastLocation(){
+        if(checkPermission()){
+            if(isLocationEnabled()){
+                fusedLPC.lastLocation.addOnCompleteListener{task ->
+                    var location = task.result
+                    if(location == null){
+                        getNewLocation()
+                    }else{
+                        txtGPS.text = "Ваши координаты:\nШирота:" + location.latitude + " ; Долгота:" + location.longitude
+                    }
+                }
+            }else{
+                Toast.makeText(this,"Сервисы, определяющие местоположение отключены.Пожалуйста, включите их", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            RequestPermission()
+        }
+    }
+
+    private fun getNewLocation(){
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 2
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLPC!!.requestLocationUpdates(
+            locationRequest,locationCallBack,Looper.myLooper()
+        )
+    }
+
+    private val locationCallBack = object: LocationCallback(){
+        override fun onLocationResult(p0: LocationResult) {
+            var lastLocation = p0.lastLocation
+            txtGPS.text = "Ваши координаты:\nШирота:" + lastLocation.latitude + " ; Долгота:" + lastLocation.longitude
+        }
     }
 }
